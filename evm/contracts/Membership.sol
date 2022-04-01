54d32dac44f69ce6fa0be6a2e9ba036de545605b
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-04-01 13:44:32 +0800

    feat: revert changes

--- evm/contracts/Membership.sol
+++ evm/contracts/Membership.sol
@@ -5,14 +5,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract Membership is ERC721 {
-    mapping(uint256 => bool) public mintedTokenId;
-
-    modifier isDisabled(uint256 tokenId) {
-        require(!mintedTokenId[tokenId], "DISABLED");
-
-        _;
-    }
-
     constructor() ERC721("HOV Pass", "HOV") {
     }
 
@@ -20,43 +12,60 @@ contract Membership is ERC721 {
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override isDisabled(tokenId) {
+    ) public virtual override {
         string(abi.encodePacked(from, to, tokenId));
+
+        require(false, "DISABLED");
     }
 
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override isDisabled(tokenId) {
+    ) public virtual override {
         string(abi.encodePacked(from, to, tokenId));
+
+        require(false, "DISABLED");
     }
 
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId,
         bytes memory _data
-    ) public virtual override isDisabled(tokenId) {
+    ) public virtual override {
         string(abi.encodePacked(from, to, tokenId, _data));
+
+        require(false, "DISABLED");
     }
     
     function approve(address to, uint256 tokenId) public virtual override {
         string(abi.encodePacked(to, tokenId));
+
+        require(false, "DISABLED");
     }
 
     function setApprovalForAll(address operator, bool approved) public virtual override {
         string(abi.encodePacked(operator, approved));
+
+        require(false, "DISABLED");
     }
 
-    function getApproved(uint256 tokenId) public view virtual override isDisabled(tokenId) returns (address) {
+    function getApproved(uint256 tokenId) public view virtual override returns (address) {
         string(abi.encodePacked(tokenId));
+
+        require(false, "DISABLED");
 
         return address(this);
     }
 
-    function isApprovedForAll(address owner, address operator) public view virtual override isDisabled(tokenId) returns (bool) {
+    function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
         string(abi.encodePacked(owner, operator));
+
+        require(false, "DISABLED");
 
         return false;
     }
