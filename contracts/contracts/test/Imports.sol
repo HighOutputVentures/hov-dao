fa0cc4df1bcb076352445205bdab31f5986993e9
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-04-07 11:19:28 +0800

    test: move imports to Imports.sol

--- contracts/contracts/test/Imports.sol
+++ contracts/contracts/test/Imports.sol
@@ -1,4 +1,5 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
 import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
